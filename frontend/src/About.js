e2d069c85565673eccf5fe9678bd54313759bd60
Fernando Duffoo <fernandojduffoo@gmail.com>
2024-08-25 10:37:15 -0400

    update

--- frontend/src/About.js
+++ frontend/src/About.js
@@ -4,7 +4,7 @@ import './About.css';
 
 function About() {
   return (
-    <Element name="about" className="section">
+    <Element name="about" className="about"> {/* Changed class name from "section" to "about" */}
       <h2>About Me</h2>
       <p>Welcome to my portfolio website! I'm Fernando Duffoo, a recent Computer Science graduate...</p>
     </Element>
